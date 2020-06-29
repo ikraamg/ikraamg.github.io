76c5fe06155d07301085d7ca9d34b292ddf504ff
Ikraam <ikraamghoor@gmail.com>
2020-06-29 17:11:56 +0200

    fixed http and added core images

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 30,
+      backSpeed: 25,
+      backDelay: 1500
     });
   }
 
